8ff5b7a781b44376b7f9a2f50cd41854ae3f96cf
KirinCupr <kirin_n@internet.ru>
2022-10-12 00:14:20 +0300

    correct work of timer

--- main.cpp
+++ main.cpp
@@ -3,10 +3,8 @@
 
 using namespace std;
 int main() {
-    int dozen = 12;
+    int delay = 12 * 60 * CLOCKS_PER_SEC;
     int choice{};
-    time_t start;
-    time_t end;
     cout << "Hello! Let's study!" << endl;
 
     cout << "Choose: studying (24 min) or break (6 min) \n1.Enter 1 to Studying\n2.Enter 2 to break";
@@ -15,28 +13,14 @@ int main() {
     cin >> choice;
 
     switch (choice)
-    {
-        case 1:
-        {
-            cout << "Time for studying!";
-
-        }
-        case 2:
-        {
-            cout << "Time for break!";
-            break;
-        }
-    }
-
-    switch (choice)
         {
             case 1:
             {
-                time(&start);
-                cout << "Time for studying!";
-                time(&end);
-                while (end - start != dozen) time(&end);
-
+                cout << "Time for studying!" << endl;
+                clock_t now = clock();
+                while(clock() - now < delay);
+                cout << "Good job!" << endl;
+                break;
             }
             case 2:
             {
