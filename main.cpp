40872ea99f7d40df91c0519d742853ff40043dc6
KirinCupr <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>
2022-11-04 20:02:39 +0300

    Add loop before case for repeating queston

--- main.cpp
+++ main.cpp
@@ -4,26 +4,41 @@
 
 using namespace std;
 int main() {
-    //12 - 12 min; 60 - 60 sec, CLOCKS_PER_SEC - ... maybe number of... something per second?..
+    //12 - 12 min; 60 - 60 sec,
+    //CLOCKS_PER_SEC - ... maybe number of... something per second?..
     //int delay = 12 * 60 * CLOCKS_PER_SEC;
     int delay = 10 * CLOCKS_PER_SEC;
     int choice{};
+    int countOfPomodoros{1};
     string activity{};
+    // buffer for writing activity for further comparing
+    string buffer{};
     ofstream fout("ListOfActivities.txt", ios::app);
+
+
     cout << "Hello! Let's study!" << endl;
 
-    cout << "Choose: studying (24 min) or break (6 min) \n1.Enter 1 to Studying\n2.Enter 2 to break";
-    cout << "\nInput: ";
+    do {
+        cout << "Choose: studying (24 min) or break (6 min) "
+                "\n1.Enter 1 to Studying"
+                "\n2.Enter 2 to break"
+                "\n3.Enter any other key to exit";
+        cout << "\nInput: ";
+        cin >> choice;
 
-    cin >> choice;
-
-    switch (choice)
+        switch (choice)
         {
             case 1:
             {
-                cout << "Please, write what kind of activity are You going to do?" << endl;
-                cin >> activity;
-                fout << activity;
+                cout << "Please, write what kind of activity are You going to do?";
+                cout << "\nInput: "; cin >> activity;
+                if (buffer == activity)
+                    countOfPomodoros++;
+                else
+                {
+                    buffer = activity;
+                    fout << '\t' << activity;
+                }
                 cout << "Time for studying!" << endl;
                 clock_t now = clock();
                 while(clock() - now < (delay * 2));
@@ -39,6 +54,9 @@ int main() {
                 break;
             }
         }
+    } while (choice == 1 || choice == 2);
+
+    fout << '\t' << countOfPomodoros << '\n';
     fout.close();
     return 0;
 }
