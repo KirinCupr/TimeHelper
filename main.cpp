e4cc8058feb1e487de4c269042168b364de6f3e6
KirinCupr <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>
2022-11-04 19:33:26 +0300

    Add file stream for writing kind of activity

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 #include <ctime>
+#include <fstream>
 
 using namespace std;
 int main() {
-    int delay = 12 * 60 * CLOCKS_PER_SEC;
+    //12 - 12 min; 60 - 60 sec, CLOCKS_PER_SEC - ... maybe number of... something per second?..
+    //int delay = 12 * 60 * CLOCKS_PER_SEC;
+    int delay = 10 * CLOCKS_PER_SEC;
     int choice{};
+    string activity{};
+    ofstream fout("ListOfActivities.txt", ios::app);
     cout << "Hello! Let's study!" << endl;
 
     cout << "Choose: studying (24 min) or break (6 min) \n1.Enter 1 to Studying\n2.Enter 2 to break";
@@ -16,6 +21,9 @@ int main() {
         {
             case 1:
             {
+                cout << "Please, write what kind of activity are You going to do?" << endl;
+                cin >> activity;
+                fout << activity;
                 cout << "Time for studying!" << endl;
                 clock_t now = clock();
                 while(clock() - now < (delay * 2));
@@ -31,6 +39,6 @@ int main() {
                 break;
             }
         }
-
+    fout.close();
     return 0;
 }
